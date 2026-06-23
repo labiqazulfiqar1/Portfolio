669837b70b4bde07e811a01a527c47a768e8abf9
Labiqa Zulfiqar <learningacc792@gmail.com>
2026-06-23 22:49:08 +0500

    Refactor property_analysis.R for clarity and correlation
    Added correlation calculation between price and bedrooms.

--- Excel and R Studio Project/property_analysis.R
+++ Excel and R Studio Project/property_analysis.R
@@ -4,7 +4,6 @@ library(readxl)
 #Upload property_data from Excel
 property_data <- read_excel("C:/Users/Technezo/Downloads/zameen-property-property_data.xlsx")
 
-#Question-1
 #Find Mean,Median,Quartiles of property Types:
 summary(property_data$price[property_data$property_type == "Flat"])
 summary(property_data$price[property_data$property_type == "Farm House"])
@@ -14,14 +13,11 @@ summary(property_data$price[property_data$property_type == "Penthouse"])
 summary(property_data$price[property_data$property_type == "Room"])
 summary(property_data$price[property_data$property_type == "House"])
 
-#Question-2
 #Find Mean,Median and Quartiles of Cities:
 summary(property_data$price[property_data$city == "Karachi"])
 summary(property_data$price[property_data$city == "Lahore"])
 summary(property_data$price[property_data$city == "Islamabad"])
 
-
-#Question-3
 #Create Scatter Plot of Price vs Bedrooms:
 plot(property_data$price, property_data$bedrooms, 
      main = "Scatter Plot of price vs bedrooms", 
